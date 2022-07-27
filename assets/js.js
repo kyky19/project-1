cefc1bca5d4fa29e7bbe5f374448134a003c0832
Andrew102803 <106626454+Andrew102803@users.noreply.github.com>
2022-07-26 19:16:28 -0500

    Update js.js
    gets location of the individal user in latitude and longitude

--- assets/js.js
+++ assets/js.js
@@ -16,3 +16,60 @@ const apiTicketKey= "4HBX1EGPuPtpVUA1BB1BxkNAwsSNstap"
   		   }
   });
 
+    
+    
+    
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+            var x = document.getElementById("location");
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+    function showPosition(position) {
+        var x = document.getElementById("location");
+        x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+        
+        var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+        $.ajax({
+          type:"GET",
+          url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&latlong="+latlon,
+          async:true,
+          dataType: "json",
+          success: function(json) {
+                      console.log(json);
+                      var e = document.getElementById("events");
+                      e.innerHTML = json.page.totalElements + " events found.";
+                      showEvents(json);
+                      initMap(position, json);
+                   },
+          error: function(xhr, status, err) {
+                      console.log(err);
+                   }
+        });
+        
+    }
+
+    function showError(error) {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                x.innerHTML = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                x.innerHTML = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                x.innerHTML = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                x.innerHTML = "An unknown error occurred."
+                break;
+        }
+    }
+    
+    
+
+
+    getLocation();
